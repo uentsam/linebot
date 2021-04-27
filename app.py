b85766f3ba1b99327672eeda9cb6516bbbfeb652
uentsam <83279526+uentsam@users.noreply.github.com>
2021-04-27 19:13:17 +0800

    Update app.py
    change the info

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/webhook", methods=['GET','POST'])
 def webhook():
     if request.method == "GET":
-        VERIFY_TOKEN = "輸入自己的驗證Token"
+        VERIFY_TOKEN = "uentsam"
         print(request)
         mode = request.args.get('hub.mode')
         sendToken = request.args.get('hub.verify_token')
@@ -33,7 +33,7 @@ def webhook():
         return "Success"
     
 def send_fb_message(to, message):
-    post_message_url = 'https://graph.facebook.com/v10.0/me/messages?access_token={token}'.format(token="這裡要放自己的Token")
+    post_message_url = 'https://graph.facebook.com/v10.0/me/messages?access_token={token}'.format(token="EAAz7DLHIXgABAMSeadr0DJpFfgFPEMZAZASvC1tW9ZCdgG1u5ZCa7P9p9wB4hN9VZA3SOYxgCm5ZBDqZAtTsw8ZCXMZCnXWZChpwgFGJUMgf402zeQYwnn6UdLFtD3FFS8H7LbW7xbZA3Oub3nknxKoR3Pmd5hmVrAnBuHu14oLqZBlgdAZDZD")
     response_message = json.dumps({"messaging_type":"RESPONSE",
                                     "recipient":{"id": to}, 
                                    "message":{"text":message}})
